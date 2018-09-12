b02097e206abbca2f143a8f4459bdfe5a00dca1c
Indexyz <jiduye@gmail.com>
2018-09-12 23:25:55 +0800

    feat(bot/command): finish args parsing
    ref: run command in new thread

--- src/java/me/indexyz/strap/Bot.java
+++ src/java/me/indexyz/strap/Bot.java
@@ -9,6 +9,7 @@ import me.indexyz.strap.utils.BotNetwork;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.SocketTimeoutException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Bot {
@@ -16,7 +17,8 @@ public class Bot {
     private Boolean enabled;
     private static Bot instance;
 
-    private Bot() {}
+    private Bot() {
+    }
 
     public static Bot get() {
         if (Bot.instance == null) {
@@ -42,6 +44,7 @@ public class Bot {
     }
 
     private static List<Class<Events>> classCache;
+
     public void start() {
         // Start pulling message
         this.enabled = true;
@@ -49,30 +52,43 @@ public class Bot {
 
         while (this.enabled) {
             try {
-                // Pulling interval
-                System.out.println(lastUpdateId);
                 List<Update> updates = this.network.getUpdates(lastUpdateId);
                 if (updates.size() != 0) {
                     lastUpdateId = updates.get(updates.size() - 1).update_id + 1;
                 }
 
                 updates.stream()
-                    .filter(i -> i.message != null)
-                    .filter(i -> i.message.text.startsWith("/"))
-                    .forEach(update -> {
-                        List<Method> methods = $.getMethods(classCache, Command.class);
-                        methods.stream().forEach(method -> {
-                            Command command = method.getAnnotation(Command.class);
-                            if (!update.message.text.startsWith(command.value())) {
-                                return;
-                            }
-                            try {
-                                method.invoke(null, update, this.network);
-                            } catch (IllegalAccessException | InvocationTargetException e) {
-                                e.printStackTrace();
-                            }
+                        .filter(i -> i.message != null)
+                        .filter(i -> i.message.text.startsWith("/"))
+                        .forEach(update -> {
+                            List<Method> methods = $.getMethods(classCache, Command.class);
+                            methods.stream().forEach(method -> {
+                                Command command = method.getAnnotation(Command.class);
+                                if (!update.message.text.startsWith(command.value())) {
+                                    return;
+                                }
+                                new Thread(() -> {
+                                    try {
+                                        if (command.parseArgs()) {
+                                            List<String> args = Arrays.asList(update.message.text.split(" "));
+
+                                            if (args.size() > 1) {
+                                                args = args.subList(1, args.size());
+                                            } else {
+                                                args = null;
+                                            }
+
+                                            method.invoke(null, update, this.network, args);
+                                        } else {
+                                            method.invoke(null, update, this.network);
+                                        }
+
+                                    } catch (IllegalAccessException | InvocationTargetException e) {
+                                        e.printStackTrace();
+                                    }
+                                }).start();
+                            });
                         });
-                    });
                 Thread.sleep(1000);
             } catch (Exception e) {
                 e.printStackTrace();
--- src/java/me/indexyz/strap/TestClass.java
+++ src/java/me/indexyz/strap/TestClass.java
@@ -9,9 +9,18 @@ import java.util.List;
 
 @Events
 public class TestClass {
-    @Command(value = "/start", parseArgs = true)
+    @Command(value = "/start")
     public static void start(Update update, BotNetwork network) {
         System.out.println(update.message.text);
         network.sendMessage(update.message.chat.id, "Hello, World!");
+    }
+
+    @Command(value = "/help", parseArgs = true)
+    public static void help(Update update, BotNetwork network, List<String> args) {
+        if (args == null) {
+            network.sendMessage(update.message.chat.id, "No args!");
+        } else {
+            network.sendMessage(update.message.chat.id, args.toString());
+        }
     }
 }
