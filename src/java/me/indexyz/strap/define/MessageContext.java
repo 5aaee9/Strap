b9147d1c68c9324528c2a5ca6b298504b6e99a66
Indexyz <jiduye@gmail.com>
2018-10-26 19:53:57 +0800

    ref: ctx.reply now can reply to message

--- src/java/me/indexyz/strap/define/MessageContext.java
+++ src/java/me/indexyz/strap/define/MessageContext.java
@@ -3,6 +3,6 @@ package me.indexyz.strap.define;
 public class MessageContext extends AbstractContext {
     public void reply(String message) {
         this.network.sendMessage
-                (this.update.message.chat.id, message);
+                (this.update.message.chat.id, message, "Markdown", false, false, this.update.message.message_id);
     }
 }
